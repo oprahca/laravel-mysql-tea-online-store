3e8c63dfdf32f007edec9b0b97ea09551a2befec
oprahca <oprah8674@gmail.com>
2022-10-08 12:10:06 -0700

    2022/10/8 add font type

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link href="/css/app.css" rel="stylesheet" />
-    <link href="http://fonts.cdnfonts.com/css/metropolis-4" rel="stylesheet">
     <title>@yield('title','Leaf Tea House')</title>
-    <style>
-        @import url('http://fonts.cdnfonts.com/css/metropolis-4');
-    </style>
 </head>
 
 
