280e316bd12bace94bc28342c97344dbdf1fddd8
oprahca <oprah8674@gmail.com>
2022-10-11 23:55:58 -0700

    2022/10/11 solved the storage path to upload into public folder

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -14,7 +14,7 @@
         </ul>
         @endif
 
-        <form method="POST" action="{{  route('admin.product.store') }}" enctype="multipart/form-data">
+        <form method="POST" action="{{  route('admin.product.store') }}">
             @csrf
             <div class="row">
                 <div class="col">
