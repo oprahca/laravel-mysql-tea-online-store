e6cd2e6d6dae04d0508f40d5ea572a2fe4de6fb6
oprahca <oprah8674@gmail.com>
2022-10-11 23:46:44 -0700

    2022/10/11 solved the storage path to upload into public folder

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('../storage/app/public'),
+            'root' => base_path('public/img/'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
